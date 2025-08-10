003f5114579fe8915d3f26d1cb9b97598c059f13
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-09 21:54:21 -0400

    create FlashCards

---
+++ flashcards/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const questions = [
+	{
+		id: 3457,
+		question: "What language is React based on?",
+		answer: "JavaScript",
+	},
+	{
+		id: 7336,
+		question: "What are the building blocks of React apps?",
+		answer: "Components",
+	},
+	{
+		id: 8832,
+		question: "What's the name of the syntax we use to describe a UI in React?",
+		answer: "JSX",
+	},
+	{
+		id: 1297,
+		question: "How to pass data from parent to child components?",
+		answer: "Props",
+	},
+	{
+		id: 9103,
+		question: "How to give components memory?",
+		answer: "useState hook",
+	},
+	{
+		id: 2002,
+		question:
+			"What do we call an input element that is completely synchronised with state?",
+		answer: "Controlled element",
+	},
+];
+
+export default function App() {
+	return (
+		<div className="flashcards">
+			<FlashCards />
+		</div>
+	);
+}
+
+function FlashCards() {
+	return (
+		<>
+			{questions.map(question => (
+				<FlashCard question={question} key={question.id} />
+			))}
+		</>
+	);
+}
+
+function FlashCard({ question }) {
+	const [isClicked, setIsClicked] = useState(false);
+
+	function handleClick() {
+		setIsClicked(bool => !bool);
+	}
+
+	return (
+		<div className={isClicked ? "selected" : ""} onClick={handleClick}>
+			{isClicked ? question.answer : question.question}
+		</div>
+	);
+}
