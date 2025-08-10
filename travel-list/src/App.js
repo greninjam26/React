e055dedfa4f6ff6ac7ebd08975a5eee2b433538e
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-10 16:04:13 -0400

    add clear list button

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -17,6 +17,11 @@ export default function App() {
 		);
 	}
 
+	function handleClearItems() {
+		const confirmed = window.confirm("Are you sure you want to delete all items?");
+		if (confirmed) setItems([]);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
@@ -25,6 +30,7 @@ export default function App() {
 				items={items}
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
+				onClearItems={handleClearItems}
 			/>
 			<Stats items={items} />
 		</div>
@@ -70,7 +76,7 @@ function Form({ onAddItems }) {
 	);
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearItems }) {
 	const [sortBy, setSortBy] = useState("input");
 
 	let sortedItems;
@@ -110,6 +116,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 					<option value="description">Sort by description</option>
 					<option value="packed">Sort by packed status</option>
 				</select>
+				<button onClick={onClearItems}>Clear list</button>
 			</div>
 		</div>
 	);
