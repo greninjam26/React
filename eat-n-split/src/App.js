8084ce0c4349945b8bb8a161fd90153855daa867
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-10 22:19:48 -0400

    build the static version of the app

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -20,5 +20,89 @@ const initialFriends = [
 ];
 
 export default function App() {
-	return <div></div>;
+	return (
+		<div className="app">
+			<div className="sidebar">
+				<FriendsList />
+				<FormAddFriend />
+				<Button>Add Friend</Button>
+			</div>
+			<FormSplitBill />
+		</div>
+	);
+}
+
+function FriendsList() {
+	const friends = initialFriends;
+	return (
+		<ul>
+			{friends.map(friend => (
+				<Friend friend={friend} key={friend.id} />
+			))}
+		</ul>
+	);
+}
+
+function Friend({ friend }) {
+	return (
+		<li>
+			<img src={friend.image} alt={friend.name} />
+			<h3>{friend.name}</h3>
+			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
+			{friend.balance > 0 && (
+				<p className="green">
+					{friend.name} owes you ${friend.balance}
+				</p>
+			)}
+			{friend.balance < 0 && (
+				<p className="red">
+					You owe {friend.name} ${Math.abs(friend.balance)}
+				</p>
+			)}
+			<Button>Select</Button>
+		</li>
+	);
+}
+
+function Button({ children }) {
+	return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+	return (
+		<form className="form-add-friend">
+			<label>👯 Friend Name</label>
+			<input type="text" />
+
+			<label>🌄 Image URL</label>
+			<input type="text" />
+
+			<Button>Add</Button>
+		</form>
+	);
+}
+
+function FormSplitBill() {
+	return (
+		<form className="form-split-bill">
+			<h2>Split a bill with Greninja</h2>
+
+			<label>💰 Bill Value</label>
+			<input type="text" />
+
+			<label>🤑 your expense</label>
+			<input type="text" />
+
+			<label>👯 Greninja's expense</label>
+			<input type="text" disabled />
+
+			<label>💵 Who is paying the bill</label>
+			<select>
+        <option value="user">You</option>
+        <option value="friend">Greninja</option>
+      </select>
+
+			<Button>Split Bill</Button>
+		</form>
+	);
 }
