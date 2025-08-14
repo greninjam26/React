38796600af63831a6cb42bdcbd18759ca5efcba1
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-14 09:14:29 -0400

    localstorage Watched List

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -7,11 +7,15 @@ const KEY = "198290e1";
 
 export default function App() {
 	const [movies, setMovies] = useState([]);
-	const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
 	const [query, setQuery] = useState("");
 	const [selectedId, setSelectedId] = useState(null);
+
+	// const [watched, setWatched] = useState([]);
+	const [watched, setWatched] = useState(function () {
+		return JSON.parse(localStorage.getItem("watched")) || [];
+	});
 
 	function handleSelectMovie(movieId) {
 		setSelectedId(id => (id === movieId ? null : movieId));
@@ -28,6 +32,13 @@ export default function App() {
 	function handleDeleteWatched(id) {
 		setWatched(watched => watched.filter(movie => movie.imdbID !== id));
 	}
+
+	useEffect(
+		function () {
+			localStorage.setItem("watched", JSON.stringify(watched));
+		},
+		[watched]
+	);
 
 	// infinite loop
 	// fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=pokemon`)
