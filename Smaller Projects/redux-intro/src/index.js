fb80f890e7a8a7e8e6be2168d23e1a08d6d30595
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-19 21:51:45 -0400

    connect React and Redux

--- Smaller Projects/redux-intro/src/index.js
+++ Smaller Projects/redux-intro/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import store from "./store";
 
 // for learning purpose this will run the code in store.js
 // import "./store";
@@ -9,6 +11,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
